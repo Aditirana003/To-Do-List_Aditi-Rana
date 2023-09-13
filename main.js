1e6a7152b4e608a929c7f1c65c842381c99491d5
Aditirana003 <101451308+Aditirana003@users.noreply.github.com>
2023-09-13 18:38:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,160 @@
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+document.getElementById("date").innerHTML = today.toDateString();
+
+function time() {
+    const data = new Date();
+    let h = data.getHours();
+    let m = data.getMinutes();
+    let s = data.getSeconds();
+
+    if(h < 10)
+        h = "0" +h;
+    if(m < 10)
+        m = "0" + m;
+    if(s < 10)
+        s = "0" + s;
+
+    document.getElementById("hour").innerHTML = h +":"+ m + ":" + s;
+    setTimeout('time()', 500);
+}
+
+
+
+
+window.addEventListener('load', () => {
+	let todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('#new-todo-form');
+	const searchInput = document.querySelector('#search-input');
+  const searchButton = document.querySelector('#search-button');
+
+	const username = localStorage.getItem('username') || '';
+
+	nameInput.value = username;
+
+	nameInput.addEventListener('change', (e) => {
+		localStorage.setItem('username', e.target.value);
+	})
+
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
+
+		todos.push(todo);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+		// Reset the form
+		e.target.reset();
+
+		DisplayTodos()
+	});
+	
+  // Add an event listener to the search button
+  searchButton.addEventListener('click', (e) => {
+	e.preventDefault();
+    const searchTerm = searchInput.value.trim().toLowerCase();
+
+    // Filter todos based on the search term and display them
+    const filteredTodos = todos.filter((todo) =>
+      todo.content.toLowerCase().includes(searchTerm)
+    );
+
+    DisplayTodos(filteredTodos);
+  });
+
+	DisplayTodos(todos);
+});
+
+function DisplayTodos (todosToDisplay = []) {
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = "";
+	
+	let todos = todosToDisplay.length > 0 ? todosToDisplay : JSON.parse(localStorage.getItem('todos')) || [];
+
+	todos.forEach((todo) => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
+
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
+
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
+
+		todoList.appendChild(todoItem);
+
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
+
+			DisplayTodos()
+
+		})
+
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
+
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter((t) => t.createdAt !== todo.createdAt);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
+
+	})
+}
